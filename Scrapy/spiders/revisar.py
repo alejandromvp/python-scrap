79839300ace5e769774611648fbb6c060bdc4cba
Nicolás Morales <nicomorales297@gmail.com>
2019-04-07 17:29:36 -0400

    prueba

--- Scrapy/spiders/revisar.py
+++ Scrapy/spiders/revisar.py
@@ -12,7 +12,9 @@ esto será para saber si hay algún producto que ya no esté disponible, o algun
 estaba disponible y volvió a estarlo. Para eso hay que buscar la forma de ver como funcionan
 las páginas cuando no se encuentra una URL en específico, que esta URL sería la que no está disponible.
 El archivo revisa-categorias.py que está en el servidor era solamente para revisar los productos que se 
-habían agregado antes y no tenían categorías o url, se le dejó una url por defecto. Así que no hay que revisar esas URL, ese archivo ya no se usará ya que desde ahora todos los productos vienen con una url, una categoría y su estado en verdadero, ese estado sólo se cambiará desde los archivos de revisar si es que no se encuentra la URL"""
+habían agregado antes y no tenían categorías o url, se le dejó una url por defecto. Así que no hay que revisar esas URL, ese archivo ya no se usará ya que desde ahora todos los productos vienen con una url, una categoría y su estado en verdadero, ese estado sólo se cambiará desde los archivos de revisar si es que no se encuentra la URL
+
+"""
 
 class RevisarSpider(scrapy.Spider):
     name = 'revisar'
